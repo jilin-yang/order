9da0da455c001d96ba96454e4e77affc1a3aa032
beihai1 <beihaixiaoge@126.com>
2020-06-13 18:04:28 +0800

    模板提提交

---
+++ GoodList.js
@@ -0,0 +1,55 @@
+module.exports = {
+  "msg": "操作成功",
+  "code": "00000",
+  "type": [
+    {
+      "name": "套餐",
+      "tid": "3",
+      "recommended": true,
+      "goods": [
+        {
+          "gid": "2",
+          "pic": "/images/1.jpg",
+          "name": "阿拉斯加烤鸡排套餐",
+          "currentPrice": "23.00",
+          "originalPrice": "28.00"
+        },
+        {
+          "gid": "1",
+          "pic": "/images/1.jpg",
+          "name": "澳洲肉眼牛排套餐",
+          "currentPrice": "28.00",
+          "originalPrice": "35.00"
+        }
+      ]
+    },
+    {
+      "name": "冰饮",
+      "tid": "2",
+      "recommended": true,
+      "goods": [
+        {
+          "gid": "4",
+          "pic": "/images/1.jpg",
+          "name": "薄荷番石榴汁",
+          "currentPrice": "12.00",
+          "originalPrice": "18.00"
+        }
+      ]
+    },
+    {
+      "name": "小吃",
+      "tid": "1",
+      "recommended": false,
+      "goods": [
+        {
+          "gid": "3",
+          "pic": "/images/1.jpg",
+          "name": "鱿鱼须",
+          "currentPrice": "23.00",
+          "originalPrice": "28.00"
+        }
+      ]
+    }
+  ]
+}
